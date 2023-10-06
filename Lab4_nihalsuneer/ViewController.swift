a9e86501391dea8aa29ab6ae3c9526b1ae64c221
user235383 <user235383@NY546.local>
2023-10-06 16:16:02 -0400

    Auto_layout

---
+++ Lab4_nihalsuneer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab4_nihalsuneer
+//
+//  Created by user235383 on 10/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Lab4_nihalsuneerUITests/Lab4_nihalsuneerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Lab4_nihalsuneerUITestsLaunchTests.swift
+//  Lab4_nihalsuneerUITests
+//
+//  Created by user235383 on 10/5/23.
+//
+
+import XCTest
+
+final class Lab4_nihalsuneerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
